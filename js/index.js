d832df11eddd3f743b17481f755869280fced266
Oleksii Kravtsov <bahbok1@gmail.com>
2022-01-08 17:33:52 -0500

    added methods for adding and removing rows. closed #3 and closed #4.

--- js/index.js
+++ js/index.js
@@ -1,32 +1,37 @@
 function print(text=""){console.log(text)}
 let grid = document.querySelector(".grid")
 let columnCount = 2
-let rows = 2
+let rowCount = 2
 setColumns()
 
 
 function addRow(){
-
+    rowCount++
+    changeCells(true, columnCount)
 }
 function removeRow(){
-
+    rowCount--
+    if(rowCount <= 0){
+        rowCount = 0
+    }
+    changeCells(false, columnCount)
 }
 
 //user adds 1 column
 function addColumn(){
-    columnCount += 1
+    columnCount++
     setColumns()
-    changeCells(true, rows)
+    changeCells(true, rowCount)
 }
 
 //user removes 1 column
 function removeColumn(){
-    columnCount -= 1
-    if(columnCount <= 1){
-        columnCount = 1
+    columnCount--
+    if(columnCount <= 0){
+        columnCount = 0
     }
     setColumns()
-    changeCells(false, rows)
+    changeCells(false, rowCount)
 }
 
 //update columns in html
@@ -35,12 +40,25 @@ function setColumns(){
 }
 
 function changeCells(mode, amount){
+    console.log("row count " + rowCount)
+    console.log("column count " + columnCount)
+    //no cells remaining
+    if(amount == 0){
+        amount = 1
+        rowCount = 1
+        columnCount = 1
+    }
+
     for(let i = 0; i < amount; i++){
         if(mode){
             addLastCell()
         }else{
             removeLastCell()
         }
+    }
+    if(grid.childElementCount == 0){
+        rowCount = 0
+        columnCount = 0
     }
 }
 
