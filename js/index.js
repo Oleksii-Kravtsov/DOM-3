88fec407700369163de2a4c4a3da7d86c3f3e433
Oleksii Kravtsov <bahbok1@gmail.com>
2022-01-08 16:31:40 -0500

    added functions for adding cells and finished functionality for adding and removing columns.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,56 @@
 function print(text=""){console.log(text)}
 let grid = document.querySelector(".grid")
 let columnCount = 2
+let rows = 2
 setColumns()
+
+
+function addRow(){
+
+}
+function removeRow(){
+
+}
 
 //user adds 1 column
 function addColumn(){
     columnCount += 1
     setColumns()
+    changeCells(true, rows)
 }
 
+//user removes 1 column
 function removeColumn(){
     columnCount -= 1
     if(columnCount <= 1){
         columnCount = 1
     }
     setColumns()
+    changeCells(false, rows)
 }
 
+//update columns in html
 function setColumns(){
     grid.style.gridTemplateColumns = `repeat(${columnCount}, 50px`
 }
 
+function changeCells(mode, amount){
+    for(let i = 0; i < amount; i++){
+        if(mode){
+            addLastCell()
+        }else{
+            removeLastCell()
+        }
+    }
+}
+
+function addLastCell(){
+    let newCell = document.createElement("button")
+    newCell.classList.add("cell")
+    grid.appendChild(newCell)
+}
+
+function removeLastCell(){
+    let children = grid.querySelectorAll(".cell")
+    grid.removeChild(children[children.length-1])
+}
