8582e6aea11016c3109e2906073b49e458ce5cb4
Oleksii Kravtsov <bahbok1@gmail.com>
2022-01-09 13:39:06 -0500

    added a lot of stuff. To be honest, I forgot about all the issuess and stuff. I really got into doing this. Sorry about that. Anyway, I got all the coloring buttons to work. I think the only thing left is the long press functionality.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,70 @@
+
 function print(text=""){console.log(text)}
+
+//grid stuff
 let grid = document.querySelector(".grid")
 let columnCount = 2
 let rowCount = 2
 setColumns()
 
+//coloring stuff
+let colorPicker = document.querySelector(".color-picker")
+let userColor = colorPicker.value
+let buttonId = 4
 
+//Color Manipulation//
+
+//set the color of one cell
+function changeColor(id){
+    let cell = document.querySelector(`#${id}`)
+    setColor(cell, userColor)
+}
+
+//fill all
+function fillAll(){
+    let cells = document.querySelectorAll(".cell")
+    setColors(cells, userColor)
+}
+
+//fill uncolored
+function fillUncolored(){
+    let unColored = document.querySelectorAll(".nocolor")
+    setColors(unColored, userColor)
+}
+
+//clear the cells back to white
+function clearAll(){
+    cells = document.querySelectorAll(".cell")
+    setColors(cells, "")
+}
+
+//update the userColor string
+function updateUserColor(){
+    userColor = colorPicker.value
+}
+
+//change the color of the individual cell
+function setColor(cell, color){
+    cell.style.backgroundColor = color
+    if(color == ""){
+        cell.classList.add("nocolor")
+    }else{
+        cell.classList.remove("nocolor")
+    }
+}
+
+//change the colors of an array of cells
+function setColors(cells, color){
+    for(let cell of cells){
+        setColor(cell, color)
+    }
+}
+
+// End //
+
+//Grid Manipulation//
+
+//user adds 1 row
 function addRow(){
     rowCount++
     setColumns()
@@ -57,19 +117,33 @@ function changeCells(mode, amount){
             removeLastCell()
         }
     }
+    //if no children remain, reset the counts
     if(grid.childElementCount == 0){
         rowCount = 0
         columnCount = 0
     }
 }
 
+//add a single cell to the end of the grid's child list
 function addLastCell(){
     let newCell = document.createElement("button")
     newCell.classList.add("cell")
+    newCell.classList.add("nocolor")
+
+    buttonId++    
+    newCell.id = "b" + buttonId
+
+    loadedFunction = changeColor.bind(this, newCell.id)
+    newCell.onclick = loadedFunction
+
     grid.appendChild(newCell)
 }
 
 function removeLastCell(){
     let children = grid.querySelectorAll(".cell")
-    grid.removeChild(children[children.length-1])
+    let child = children[children.length-1]
+    buttonId--
+    grid.removeChild(child)
 }
+
+// End //
